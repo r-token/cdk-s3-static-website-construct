55276cb9ed6880fcce8817a0f7a3aaab703434c3
Ryan Token <ryantoken13@icloud.com>
2021-04-17 16:43:58 -0500

    Initial commit

---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+import * as cdk from '@aws-cdk/core';
+
+export interface S3StaticWebsiteConstructProps {
+  // Define construct properties here
+}
+
+export class S3StaticWebsiteConstruct extends cdk.Construct {
+
+  constructor(scope: cdk.Construct, id: string, props: S3StaticWebsiteConstructProps = {}) {
+    super(scope, id);
+
+    // Define construct contents here
+  }
+}
